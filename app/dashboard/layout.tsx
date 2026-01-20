039076a36ba5d1093874f63c9947eb731baa93b5
Piyush Tiwari <piyushtiwarindls220@gmail.com>
2026-01-20 23:58:40 +0530

    theme toggle and dashboard layout

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  SidebarProvider,
+  SidebarInset,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { Separator } from "@/components/ui/separator";
+import { AppSidebar } from "@/components/app-sidebar";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mx-2 h-4" />
+          <h1 className="text-xl font-semibold text-foreground">Dashboard</h1>
+        </header>
+        <main className="flex-1 overflow-auto p-4 md:p-6">{children}</main>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+};
+
+export default DashboardLayout;
--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,9 @@ import { Button } from "@/components/ui/button";
 import Logout from "@/module/auth/component/logout";
 import { requireAuth } from "@/module/auth/utils/auth-utils";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   await requireAuth();
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <Logout>
-        <Button className="cursor-pointer">Logout</Button>
-      </Logout>
-    </div>
-  );
+  return redirect("/dashboard");
 }
